0de778b5b324534efcceeaf243427d1675b3369d
Jenny Jin <yue_jin@apple.com>
2015-07-03 03:01:13 -0700

    sanity test 0. very basic. no product features.

--- predictor.py
+++ predictor.py
@@ -5,39 +5,31 @@ import sys
 import recsys.algorithm
 recsys.algorithm.VERBOSE = True
 
-from recsys.algorithm.factorize import SVD
+from recsys.algorithm.factorize import SVD, SVDNeighbourhood
 from recsys.datamodel.data import Data
 from recsys.evaluation.prediction import RMSE, MAE
 
-# svd = pickle.load(open('model/svd.obj', 'r'))
-svd = SVD()
-svd.load_model('./model/svd.obj.zip')
+from main import evaluate
 
-#Dataset
-PERCENT_TRAIN = 0
-data = Data()
-data.load('/Users/jennyyuejin/recommender/Data/u.data',
-          sep='\t',
-          format={'col':0, 'row':1, 'value':2, 'ids':int})
-# About format parameter:
-#   'row': 1 -> Rows in matrix come from column 1 in ratings.dat file
-#   'col': 0 -> Cols in matrix come from column 0 in ratings.dat file
-#   'value': 2 -> Values (Mij) in matrix come from column 2 in ratings.dat file
-#   'ids': int -> Ids (row and col ids) are integers (not strings)
+if __name__ == '__main__':
 
-#Train & Test data
-_, test = data.split_train_test(percent=PERCENT_TRAIN, shuffle_data=False)
+    svd = SVDNeighbourhood()
+    # svd = SVD()
+    svd.load_model('./model/svd.obj.zip')
 
-#Evaluation using prediction-based metrics
-rmse = RMSE()
-mae = MAE()
-for rating, item_id, user_id in test.get():
-    try:
-        pred_rating = svd.predict(item_id, user_id)
-        rmse.add(rating, pred_rating)
-        mae.add(rating, pred_rating)
-    except KeyError:
-        continue
+    #Dataset
+    PERCENT_TRAIN = 100
+    data = Data()
+    data.load('/Users/jennyyuejin/recommender/Data/movieData/u.data',
+              sep='\t',
+              format={'col':0, 'row':1, 'value':2, 'ids':int})
+    # About format parameter:
+    #   'row': 1 -> Rows in matrix come from column 1 in ratings.dat file
+    #   'col': 0 -> Cols in matrix come from column 0 in ratings.dat file
+    #   'value': 2 -> Values (Mij) in matrix come from column 2 in ratings.dat file
+    #   'ids': int -> Ids (row and col ids) are integers (not strings)
 
-print 'RMSE=%s' % rmse.compute()
-print 'MAE=%s' % mae.compute()
+    #Train & Test data
+    _, test = data.split_train_test(percent=PERCENT_TRAIN, shuffle_data=True)
+
+    evaluate(svd, test)
--- test_0.py
+++ test_0.py
@@ -9,18 +9,19 @@ from recsys.evaluation.prediction import RMSE, MAE
 
 
 #Evaluation using prediction-based metrics
-def evaluate(_svd, _testData):
+def evaluate(_svd, _testData, verbose=False):
     global rmse, mae, rating, item_id, user_id, pred_rating
     rmse = RMSE()
     mae = MAE()
     for rating, item_id, user_id in _testData.get():
         try:
-            pred_rating = _svd.predict(item_id, user_id)
+            pred_rating = _svd.predict(item_id, user_id, MIN_VALUE=0, MAX_VALUE=10)
             rmse.add(rating, pred_rating)
             mae.add(rating, pred_rating)
 
-            # print item_id, user_id, rating, pred_rating
-        except KeyError as e:
+            if verbose:
+                print item_id, user_id, rating, pred_rating
+        except Exception as e:
             print 'ERROR occurred:', e.message
 
     print 'RMSE=%s' % rmse.compute()
@@ -30,9 +31,9 @@ def evaluate(_svd, _testData):
 if __name__ == '__main__':
 
     #Dataset
-    PERCENT_TRAIN = 50
+    PERCENT_TRAIN = 100
     data = Data()
-    data.load('/Users/jennyyuejin/recommender/Data/movieData/u.data',
+    data.load('/Users/jennyyuejin/recommender/Data/test_0/userProd.data',
               sep='\t',
               format={'col':0, 'row':1, 'value':2, 'ids':int})
     # About format parameter:
@@ -50,25 +51,33 @@ if __name__ == '__main__':
     # svd = SVDNeighbourhood()
     svd = SVD()
     svd.set_data(train)
-    svd.compute(k=K, min_values=5, pre_normalize=None, mean_center=True, post_normalize=True)
+    svd.compute(k=K, min_values=None, pre_normalize=None, mean_center=True, post_normalize=True)
 
     # save
     # svd.set_data(None)  # clear data before saving
     # pickle.dump(svd, open('./model/svd.obj', 'w'))
     svd.save_model('./model/svd.obj.zip',
-        {'k': K, 'min_values': 5,
-         'pre_normalize': None, 'mean_center': True, 'post_normalize': True})
+                   {'k': K, 'min_values': 5,
+                    'pre_normalize': None, 'mean_center': True, 'post_normalize': True})
 
 
     print '------ evaluating original'
-    evaluate(svd, test)
 
-    # svd_pred = SVDNeighbourhood()
-    svd_pred = SVD()
-    svd_pred.load_model('./model/svd.obj.zip')
+    # similarity between items x and y
+    svd.similarity(1, 3)
 
-    print '------ evaluating copy'
-    evaluate(svd_pred, test)
+    # similar to item x
+    svd.similar(1)
 
+    # predict ratings
+    evaluate(svd, test, True)
 
+    # recommend products to a user
+    for userid in [0, 1, 2]:
+        print 'User #', userid
+        print svd.recommend(userid, is_row=False, only_unknowns=True)
 
+    # which users should use a given product?
+    for prodid in [0, 1, 3, 4]:
+        print 'Product #', prodid
+        print svd.recommend(prodid, only_unknowns=True)
